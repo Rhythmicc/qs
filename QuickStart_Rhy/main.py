2d837854d243e8a7e4532ace9faa135f6f6af728
连浩丞 <RhythmLian@outlook.com>
2019-12-22 14:10:14 +0800

    support to download *.m3u8

--- QuickStart_Rhy/main.py
+++ QuickStart_Rhy/main.py
@@ -20,8 +20,8 @@ arlen = len(sys.argv)
 
 def get_ip():
     import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
         return ip
@@ -123,6 +123,8 @@ def open_file():
 
 
 def init():
+    wb.open('http://login.cup.edu.cn')
+    return
     root = os.path.expanduser('~') + dir_char
     flag = False
     try:
@@ -138,20 +140,21 @@ def init():
     data = {
         'action': 'login',
         'ac_id': '1',
-        #'user_ip': get_ip(),
+        'user_ip': get_ip(),
         'nas_ip': '',
         'user_mac': '',
         'url': '',
         'username': user,
-        'password': pwd,
+        'password': '{MD5}'+pwd,
         'remember': '1'
     }
 
-    html = requests.post('http://login.cup.edu.cn/cgi-bin/rad_user_info', data=data, headers=headers)
+    html = requests.post('http://login.cup.edu.cn/cgi-bin/srun_portal', data=data, headers=headers)
     if html:
         html = html.text
     else:
         exit('未接入CUP校园网')
+    print(html)
     if 'error' in html:
         print('登录失败, 可以进行以下操作:\n\t* 尝试网络缴费后登录\n\t* 重新尝试登录')
         remove(root + '.wifirc')
@@ -288,6 +291,107 @@ def cur_time():
     print(' '.join(tm))
 
 
+def m3u8_dl(url):
+    import threading
+    import urllib3
+    import shutil
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    def merge_file(path, name):
+        if not os.path.exists(path):
+            return
+        ls = os.listdir(path)
+        if not path.endswith(dir_char):
+            path += dir_char
+        tsls = []
+        for i in ls:
+            if i.endswith('.ts'):
+                tsls.append(path + i)
+        tsls.sort()
+        with open(name + '.ts', 'wb') as f:
+            for i in tsls:
+                with open(i, 'rb') as ff:
+                    f.write(ff.read())
+
+    class M3U8DL:
+        class _dl(threading.Thread):
+            from Crypto.Cipher import AES
+
+            def __init__(self, job_ls, mutex, path='./'):
+                threading.Thread.__init__(self)
+                self.job_ls = job_ls
+                self.mutex = mutex
+                self.path = path
+
+            def run(self):
+                key = ''
+                while self.job_ls:
+                    self.mutex.acquire()
+                    job = self.job_ls.pop()
+                    self.mutex.release()
+                    if not job[-1]:
+                        uri_pos = job[1].find("URI")
+                        quotation_mark_pos = job[1].rfind('"')
+                        key_path = job[1][uri_pos:quotation_mark_pos].split('"')[1]
+                        key_url = job[0] + key_path
+                        res = requests.get(key_url, verify=False)
+                        key = res.content
+                    else:
+                        pd_url = job[0]
+                        c_fule_name = job[1]
+                        if os.path.exists(os.path.join(self.path, c_fule_name)):
+                            continue
+                        res = requests.get(pd_url, verify=False)
+                        if len(key):
+                            cryptor = self.AES.new(key, self.AES.MODE_CBC, key)
+                            with open(os.path.join(self.path, c_fule_name + ".mp4"), 'ab') as f:
+                                f.write(cryptor.decrypt(res.content))
+                        else:
+                            with open(os.path.join(self.path, c_fule_name), 'ab') as f:
+                                f.write(res.content)
+                                f.flush()
+
+        def __init__(self, target, name, thread_num):
+            self.target = target
+            self.name = name
+            self.th_num = thread_num
+
+        def download(self):
+            target = self.target
+            download_path = os.getcwd() + dir_char + self.name
+            if not os.path.exists(download_path):
+                os.mkdir(download_path)
+            all_content = requests.get(target, verify=False).text
+            if "#EXTM3U" not in all_content:
+                raise BaseException("非M3U8的链接")
+            if "EXT-X-STREAM-INF" in all_content:
+                file_line = all_content.split("\n")
+                for line in file_line:
+                    if '.m3u8' in line:
+                        target = target.rsplit("/", 1)[0] + "/" + line
+                        all_content = requests.get(target, verify=False).text
+            file_line = all_content.split("\n")
+            rt = target.rsplit("/", 1)[0] + "/"
+            tmp = []
+            for index, line in enumerate(file_line):
+                if "#EXT-X-KEY" in line:
+                    tmp.append((rt, line, 0))
+                if "EXTINF" in line:
+                    tmp.append((rt + file_line[index + 1], file_line[index + 1].rsplit("/", 1)[-1], 1))
+            file_line = tmp
+            mutex = threading.Lock()
+            tls = []
+            for i in range(self.th_num):
+                tls.append(self._dl(file_line, mutex, download_path))
+                tls[-1].start()
+            for i in tls:
+                i.join()
+            print("Download completed!")
+            merge_file(download_path, self.name)
+            shutil.rmtree(download_path)
+    M3U8DL(url, url.split('.')[-2], 16).download()
+
+
 def download():
     urls = sys.argv[2:]
     if not urls:
@@ -295,13 +399,16 @@ def download():
         urls = pyperclip.paste().split()
     if urls:
         for url in urls:
-            package = requests.get(url, headers).content
-            if package:
-                file_name = url.split('/')[-1]
-                with open(file_name, 'wb') as f:
-                    f.write(package)
+            if url.endswith('m3u8'):
+                m3u8_dl(url)
             else:
-                print('Download "%s" failed!' % url)
+                package = requests.get(url, headers).content
+                if package:
+                    file_name = url.split('/')[-1]
+                    with open(file_name, 'wb') as f:
+                        f.write(package)
+                else:
+                    print('Download "%s" failed!' % url)
     else:
         print("No url found!")
 
