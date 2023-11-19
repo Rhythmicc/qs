e4012b2bf323600a3998f68b63aaa74e2ac5b724
Rhythmicc <rhythmlian.cn@gmail.com>
2023-11-20 02:41:13 +0800

    support poe api

--- QuickStart_Rhy/API/GPT/__init__.py
+++ QuickStart_Rhy/API/GPT/__init__.py
@@ -7,7 +7,7 @@ def ChatGPT(prompt: str):
 
     if not _chatbot:
         _config = qs_config.basicSelect("gpt")
-        api_supporter = _config["support"][_config["index"]]
+        api_supporter = _config["index"]
         model = _config["model"]
         _chatbot = requirePackage('.API.GPT.' + api_supporter, 'create_bot')(model)
     
